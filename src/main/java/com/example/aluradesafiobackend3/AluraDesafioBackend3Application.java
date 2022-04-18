5edccaffcb2ae7789c1cba8d1f12a2a64581cad1
Rodrigo Pimenta <rodrigo.pimenta@ext.collectivehealth.com>
2022-04-18 16:35:47 -0300

    first commit

---
+++ src/main/java/com/example/aluradesafiobackend3/AluraDesafioBackend3Application.java
@@ -0,0 +1,13 @@
+package com.example.aluradesafiobackend3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraDesafioBackend3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AluraDesafioBackend3Application.class, args);
+    }
+
+}
